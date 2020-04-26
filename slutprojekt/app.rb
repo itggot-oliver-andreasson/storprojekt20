beb582104344bcf5bc39bd1012b9036d52bdbd7c
itggot-oliver-andreasson <59963478+itggot-oliver-andreasson@users.noreply.github.com>
2020-04-26 14:44:25 +0200

    update, remove and yardoc

--- slutprojekt/app.rb
+++ slutprojekt/app.rb
@@ -5,16 +5,27 @@ require 'bcrypt'
 require 'byebug'
 require_relative 'model.rb'
 
+include Model
+
 enable :sessions
 
 db = SQLite3::Database.new('db/database.db')
 db.results_as_hash = true
 
-
+# Displays register page
+# 
 get('/register') do
   	slim(:"users/register")
 end
 
+# Registers an account
+#
+# @param [String] :password, users password
+# @param [String] :passwordconf, confirmation of users password
+# @param [String] :username, users username
+# 
+# @see Model#userexist
+# @see Model#createuser
 post('/registeredaccount') do
   	password = params[:password]
   	confirm = params[:passwordconf]
@@ -31,10 +42,19 @@ post('/registeredaccount') do
   	redirect('/login')
 end
 
+# Displays login page
+#
 get('/login') do
 	slim(:"users/login")
 end
 
+# Confirms the login of user
+#
+# @param [String] :username, users username
+# @param [String] :password, users password
+# 
+# @see Model#userexist
+# @see Model#login
 post('/loginconfirm') do
 	username = params[:username]
 	password = params[:password]
@@ -52,14 +72,23 @@ post('/loginconfirm') do
   	end 
 end
 
+# Displays error page
+#
 get("/error") do
 	slim(:error)
 end
 
+# Shows landing page
+#
 get("/") do
   slim(:index)
 end
 
+# Displays recipe page
+#
+# @see Model#user_info
+# @see Model#getrecipes
+# @see Model#getingnames
 get("/recipes") do
 	if session[:user_id] == nil
 		session[:error] = "You are not logged in. Log in to view all recipes!"
@@ -73,11 +102,16 @@ get("/recipes") do
 	end
 end
 
+# Logs out the user and clears sessions
+# 
 post("/logoutform") do
 	session.clear
 	redirect("/")
 end
 
+# Displays profile page of logged in user
+#
+# @see Model#user_info
 get("/profile") do
 	if session[:user_id] == nil
 		session[:error] = "You are not logged in. Log in to view your profile!"
@@ -89,6 +123,10 @@ get("/profile") do
 	end
 end
 
+# Displays administrator settings
+# 
+# @see Model#user_info
+# @see Model#users
 get("/administrator") do
 	if session[:user_id] == nil
 		session[:error] = "You are not logged in. Log in to view your profile!"
@@ -104,6 +142,12 @@ get("/administrator") do
 	end
 end
 
+# Deletes a user by administrator
+#
+# @param [String] :id, id of deleted user
+# 
+# @see Model#potdeluserinfo
+# @see Model#user_delete
 post("/administrator/:id/deleteuser") do
     user_delete_id = params[:id]
     user_info = potdeluserinfo(session[:user_id], user_delete_id)
@@ -123,6 +167,8 @@ post("/administrator/:id/deleteuser") do
     end
 end
 
+# Displays the page for uploading avatars
+#
 get("/newavatar") do
 	if session[:user_id] == nil
 		session[:error] = "You are not logged in. Log in to change avatar!"
@@ -133,6 +179,12 @@ get("/newavatar") do
 	end
 end
 
+# Uploads and changes to new avatar for user
+#
+# @param [String] :file, name of file
+# 
+# @see Model#fileupload
+# @see Model#avatarchange
 post("/avataruploaded") do
 	file = fileupload(params[:file])
 	if !file
@@ -157,6 +209,9 @@ post("/avataruploaded") do
     redirect('/profile')
 end
 
+# Displays creation of recipe page
+# 
+# @see Model#getings
 get("/createrecipe") do
 	if session[:user_id] == nil
 		session[:error] = "You are not logged in. Log in create a recipe!"
@@ -174,6 +229,16 @@ get("/createrecipe") do
 	end
 end
 
+# Creates a recipe
+#
+# @param [Integer] :ingnum, number of ingredients
+# @param [String] :file, name of file
+# @param [String] :title, title of recipe
+# @param [String] :desc, description/instructions to ingredients
+# 
+# @see Model#getings
+# @see Model#fileupload
+# @see Model#createrecipe
 post("/recipecreated") do
 	ingnum = params[:ingnum].to_i
 	ings = []
@@ -218,5 +283,104 @@ post("/recipecreated") do
         redirect('/error')
     end
 	createrecipe(title, desc, file[:name], ings, measures, units, session[:user_id])
+	redirect("/recipes")
+end
+
+# Deletes a users recipe either by owner or admin
+#
+# @param [Integer] :id, id of recipe
+# 
+# @see Model#userrecipe
+# @see Model#deletepost
+post("/recipes/:id/delete") do
+	recipe_id = params[:id]
+    userrecipeinfo = userrecipe(session[:user_id], recipe_id)
+    if userrecipeinfo[1][0]["user_id"] == session[:user_id] || userrecipeinfo[0][0]["admin"] == "1"
+        deletepost(recipe_id)
+    else
+        session[:error] = "That is not your post or you are not an admin."
+        session[:route] = "/recipes"
+        redirect("/error")
+    end
+    redirect("/recipes")
+end
+
+# Displays recipe-updation page
+#
+# @param [Integer] :id, id of recipe
+# 
+# @see Model#userrecipe
+# @see Model#getings
+# @see Model#recipe_ing_rel
+get("/recipes/:id/update") do
+    recipe_id = params[:id]
+	userrecipeinfo = userrecipe(session[:user_id], recipe_id)
+	ingredients = getings()
+	recipe_ing_rel = recipe_ing_rel(recipe_id)
+    if userrecipeinfo[1][0]["user_id"] == session[:user_id]
+		slim(:"main/update", locals: {userrecipeinfo: userrecipeinfo[1][0], recipe_id: recipe_id, ingredients: ingredients, recipe_ing_rel: recipe_ing_rel})
+    else
+        session[:error] = "You are not the owner of this post."
+        session[:route] = "/recipes"
+        redirect("/error")
+    end
+end
+
+# Updates the recipe by owner
+#
+# @param [Integer] :id, id of recipe
+# @param [Integer] :ingnum, number of ingredients
+# @param [String] :title, title of recipe
+# @param [String] :desc, description/instructions to ingredients
+# @param [String] :file, name of file uploaded
+# 
+# @see Model#getings
+# @see Model#fileupload
+# @see Model#updaterecipe
+post("/recipes/:id/updatedrecipe") do
+	recipe_id = params[:id]
+	ingnum = params[:ingnum].to_i
+	ings = []
+	measures = []
+	units = []
+	ingreds = getings()
+	(1..ingnum).each do |i|
+		ingreds.each do |ingred|
+			if params[:"ingredient#{i}"] == ingred["ing_name"]
+				ings << ingred["id"]
+			end
+		end
+		measures << params[:"amount#{i}"]
+		if params[:"measurement#{i}"] == "ml"
+			units << "1"
+		elsif params[:"measurement#{i}"] == "dash/es"
+			units << "2"
+		else
+			session[:error] = "You used a wrong measurement!"
+			session[:route] = "/createrecipe"
+			redirect("/error")
+		end
+	end
+	title = params[:title]
+	desc = params[:desc]
+	file = fileupload(params[:file])
+	if !file
+		session[:error] = "Wrong file extension. Images only!"
+		session[:route] = "/createrecipe"
+		File.delete(file[:target])
+		redirect("/error")
+	end
+	target = file[:target]
+    tempfile = file[:tempfile]
+    name = file[:name]
+	File.open(target, 'wb') {|f| f.write tempfile.read }
+    filesize = File.size(target)
+    if File.size(target) > 5000000
+        File.delete(target)
+        session[:error] = "That file is to big. We only allow max 5MB images."
+        session[:route] = "/createrecipe"
+        redirect('/error')
+    end
+	updaterecipe(title, desc, file[:name], ings, measures, units, session[:user_id], recipe_id)
 	redirect("/recipes")
 end
--- slutprojekt/model.rb
+++ slutprojekt/model.rb
@@ -1,89 +1,226 @@
-def db()
-    db = SQLite3::Database.new("db/database.db")    
-    db.results_as_hash = true
-    return db
-end
+# Contains all code for database
+# 
+module Model
 
-def createuser(password, username, admin)
-    password_digest = BCrypt::Password.create(password)
-    db.execute("INSERT INTO users(password, username, administrator) VALUES (?,?,?)", [password_digest, username, admin])
-end
-
-def userexist(username)
-    return db.execute("SELECT id FROM users WHERE username=?", username)
-end
-
-def login(username, password)
-    password_digest = db.execute("SELECT password FROM users WHERE username=?", username)
-    if BCrypt::Password.new(password_digest[0][0]) == password
-        session[:username] = username
-        session[:user_id] = db.execute("SELECT id FROM users WHERE username=?", username)[0][0]
-        return true
+    # Makes the use of db.execute work
+    # 
+    # @return [Database] containing database
+    def db()
+        db = SQLite3::Database.new("db/database.db")    
+        db.results_as_hash = true
+        return db
     end
-end
-
-def user_info(user_id)
-    return db.execute("SELECT * FROM users WHERE id=?", user_id)
-end
-
-def users(user_id)
-    return [db.execute("SELECT * FROM users"), db.execute("SELECT * FROM users WHERE id=?", user_id)]
-end
-
-def potdeluserinfo(user_id, user_delete_id)
-    return [db.execute("SELECT * FROM users WHERE id=?", user_id), db.execute("SELECT * FROM users WHERE id=?", user_delete_id)]
-end
-
-def user_delete(user_delete_id)
-    db.execute("DELETE FROM users WHERE id=?", user_delete_id)
-end
-
-def fileupload(filename)
-    unless filename &&
-        (tempfile = filename[:tempfile]) &&
-        (name = filename[:filename])
-    @error = "No file selected"
-    return false
+    
+    # Creates a user
+    # 
+    # @param [String] password, contains password
+    # @param [String] username, contains username
+    # @param [String] admin, contains admin
+    #  
+    # @return [Nil]
+    def createuser(password, username, admin)
+        password_digest = BCrypt::Password.create(password)
+        db.execute("INSERT INTO users(password, username, administrator) VALUES (?,?,?)", [password_digest, username, admin])
     end
-    print "File #{filename[:filename]} uploaded"
-    fileextension = filename["filename"]
-    if File.extname(fileextension) == ".png" || File.extname(fileextension) == ".jpg" || File.extname(fileextension) == ".jpeg"
-        target = "public/img/#{name}"
-        file = {target: target, name: name, tempfile: tempfile}
-        return file
-    else
+    
+    # Checks if user exists
+    # 
+    # @param [String] username, contains username
+    #  
+    # @return [Integer] containing id of user
+    def userexist(username)
+        return db.execute("SELECT id FROM users WHERE username=?", username)
+    end
+    
+    # Checks if password is correct
+    # 
+    # @param [String] username, contains username
+    # @param [String] password, contains password
+    #  
+    # @return [Boolean] containing true if password correct
+    def login(username, password)
+        password_digest = db.execute("SELECT password FROM users WHERE username=?", username)
+        if BCrypt::Password.new(password_digest[0][0]) == password
+            session[:username] = username
+            session[:user_id] = db.execute("SELECT id FROM users WHERE username=?", username)[0][0]
+            return true
+        end
+    end
+    
+    # Gets all userinfo about a user
+    # 
+    # @param [Integer] user_id, contains id of user
+    #  
+    # @return [Hash] containing all info about user
+    def user_info(user_id)
+        return db.execute("SELECT * FROM users WHERE id=?", user_id)
+    end
+    
+    # Returns info about all users
+    # 
+    # @param [Integer] user_id, contains id of user
+    #  
+    # @return [Array] containing info about users
+    def users(user_id)
+        return [db.execute("SELECT * FROM users"), db.execute("SELECT * FROM users WHERE id=?", user_id)]
+    end
+    
+    # Returns info about potential deleted user and admin
+    # 
+    # @param [Integer] user_id, contains id of user
+    # @param [Integer] user_delete_id, contains id of potential deleted user
+    #  
+    # @return [Array] containing the information of admin and potential deleted user
+    def potdeluserinfo(user_id, user_delete_id)
+        return [db.execute("SELECT * FROM users WHERE id=?", user_id), db.execute("SELECT * FROM users WHERE id=?", user_delete_id)]
+    end
+    
+    # Deletes a user
+    # 
+    # @param [Integer] user_delete_id, contains id of deleted user
+    #  
+    # @return [Nil]
+    def user_delete(user_delete_id)
+        db.execute("DELETE FROM users WHERE id=?", user_delete_id)
+    end
+    
+    # Uploads a file to the server
+    # 
+    # @param [String] filename, contains name of file
+    #  
+    # @return [Hash] containing file information
+    def fileupload(filename)
+        unless filename &&
+            (tempfile = filename[:tempfile]) &&
+            (name = filename[:filename])
+        @error = "No file selected"
         return false
+        end
+        print "File #{filename[:filename]} uploaded"
+        fileextension = filename["filename"]
+        if File.extname(fileextension) == ".png" || File.extname(fileextension) == ".jpg" || File.extname(fileextension) == ".jpeg"
+            target = "public/img/#{name}"
+            file = {target: target, name: name, tempfile: tempfile}
+            return file
+        else
+            return false
+        end
     end
-end
-
-def avatarchange(user_id, name)
-    db.execute("UPDATE users SET avatar=? WHERE id=?", name, user_id)
-end
-
-def getings()
-    return db.execute("SELECT * FROM ingredients")
-end
-
-def createrecipe(title, desc, filename, ingredients, measurements, units, user_id)
-    ingstr = ""
-    measurestr = ""
-    unitstr = ""
-    (0..(ingredients.length-1)).each do |i|
-        ingstr += ingredients[i].to_s + ","
-        measurestr += measurements[i].to_s + ","
-        unitstr += units[i].to_s + ","
+    
+    # Changes avatar for user
+    # 
+    # @param [Integer] user_id, contains id of user
+    # @param [String] name, contains name of image
+    #  
+    # @return [Nil]
+    def avatarchange(user_id, name)
+        db.execute("UPDATE users SET avatar=? WHERE id=?", name, user_id)
     end
-    ingstr = ingstr.chop
-    measurestr = measurestr.chop
-    unitstr = unitstr.chop
-    db.execute("INSERT INTO recipes(user_id, name, img, desc) VALUES (?,?,?,?)", [user_id, title, filename, desc])
-    db.execute("INSERT INTO recipe_to_ing_rel(recipe_id, ing_id, amount, unit) VALUES (?,?,?,?)", [db.execute("SELECT recipe_id FROM recipes ORDER BY recipe_id DESC LIMIT 1")[0]["recipe_id"], ingstr, measurestr, unitstr])
-end
-
-def getrecipes()
-    return [db.execute("SELECT * FROM recipes"), db.execute("SELECT * FROM recipe_to_ing_rel")]
-end
-
-def getingnames()
-    return db.execute("SELECT ing_name FROM ingredients")
-end
+    
+    # Gets all ingredients from database
+    # 
+    # @return [Hash] containing ingredients
+    def getings()
+        return db.execute("SELECT * FROM ingredients")
+    end
+    
+    # Creates a recipe
+    # 
+    # @param [String] title, contains title of recipe
+    # @param [String] desc, contains description/instructions for recipe
+    # @param [String] filename, contains name of file
+    # @param [Array] ingredients, contains all ingredients 
+    # @param [Array] measurements, contains all measurements
+    # @param [Array] contains units of all ingredients
+    # @param [Integer] contains id of users
+    #  
+    # @return [Nil]
+    def createrecipe(title, desc, filename, ingredients, measurements, units, user_id)
+        ingstr = ""
+        measurestr = ""
+        unitstr = ""
+        (0..(ingredients.length-1)).each do |i|
+            ingstr += ingredients[i].to_s + ","
+            measurestr += measurements[i].to_s + ","
+            unitstr += units[i].to_s + ","
+        end
+        ingstr = ingstr.chop
+        measurestr = measurestr.chop
+        unitstr = unitstr.chop
+        db.execute("INSERT INTO recipes(user_id, name, img, desc) VALUES (?,?,?,?)", [user_id, title, filename, desc])
+        db.execute("INSERT INTO recipe_to_ing_rel(recipe_id, ing_id, amount, unit) VALUES (?,?,?,?)", [db.execute("SELECT recipe_id FROM recipes ORDER BY recipe_id DESC LIMIT 1")[0]["recipe_id"], ingstr, measurestr, unitstr])
+    end
+    
+    # Gets all recipes
+    # 
+    # @return [Array] containing all recipes and recipe-ingredient-relation
+    def getrecipes()
+        return [db.execute("SELECT * FROM recipes"), db.execute("SELECT * FROM recipe_to_ing_rel")]
+    end
+    
+    # Gets all ingredient names
+    # 
+    # @return [Array] containing all ingredient names
+    def getingnames()
+        return db.execute("SELECT ing_name FROM ingredients")
+    end
+    
+    # Gets info about user and recipes
+    # 
+    # @param [Integer] user_id, contains id of user
+    # @param [Integer] recipe_id, contains id of recipe
+    #  
+    # @return [] containing
+    def userrecipe(user_id, recipe_id)
+        return [db.execute("SELECT * FROM users WHERE id=?", user_id), db.execute("SELECT * FROM recipes WHERE recipe_id=?", recipe_id)]
+    end
+    
+    # Deletes a recipe
+    # 
+    # @param [Integer] recipe_id, contains id of recipe
+    #  
+    # @return [Nil]
+    def deletepost(recipe_id)
+        db.execute("DELETE FROM recipes WHERE recipe_id=?", recipe_id)
+    end
+    
+    # Gets the recipe-ingredient-relation table
+    # 
+    # @param [Integer] recipe_id, contains id of recipe
+    #  
+    # @return [Hash] containing recipe-ingredient-relation table
+    def recipe_ing_rel(recipe_id)
+        return db.execute("SELECT * FROM recipe_to_ing_rel WHERE recipe_id=?", recipe_id)
+    end
+    
+    # Updates the recipe by user
+    # 
+    # @param [String] title, contains the name of recipe
+    # @param [String] desc, contains the description/instructions for recipe
+    # @param [String] filename, contains name of image
+    # @param [Array] ingredients, contains all ingredients
+    # @param [Array] measurements, contains all measurements for ingredients
+    # @param [Array] units, contains all units for measurements
+    # @param [Integer] user_id, contains id of user
+    # @param [Integer] recipe_id, contains id of recipe
+    #  
+    # @return [Nil]
+    def updaterecipe(title, desc, filename, ingredients, measurements, units, user_id, recipe_id)
+        ingstr = ""
+        measurestr = ""
+        unitstr = ""
+        (0..(ingredients.length-1)).each do |i|
+            ingstr += ingredients[i].to_s + ","
+            measurestr += measurements[i].to_s + ","
+            unitstr += units[i].to_s + ","
+        end
+        byebug
+        ingstr = ingstr.chop
+        measurestr = measurestr.chop
+        unitstr = unitstr.chop
+        db.execute("UPDATE recipes SET user_id=?, name=?, img=?, desc=? WHERE recipe_id=?", user_id, title, filename, desc, recipe_id)
+        db.execute("UPDATE recipe_to_ing_rel SET ing_id=?, amount=?, unit=? WHERE recipe_id=?", ingstr, measurestr, unitstr, recipe_id)
+    end
+    
+    end
+        
